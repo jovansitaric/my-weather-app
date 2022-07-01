158858d56ba497516f8f7b7be07a506744e7f6e1
jsitaric <jsitaric@gmail.com>
2022-07-01 11:56:37 +0200

    Finished with input location fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location},rs&units=metric&APPID=d27154f713f52f98786a7ee263e927c2`;
+
+    const searchLocation = (event) => {
+
+        event.preventDefault();
+
+        axios.get(url).then((response) => {
+            setData(response.data);
+            console.log(response.data);
+        });
+        setLocation('');
+    };
+
+    return (
+        <form className="app" onSubmit={searchLocation}>
+            <input
+                type="text"
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
+                placeholder="Enter Location"
+            />
+            <div className="container">
+                <div className="top">
+                    <div className="location">
+                        <p>{data.name}</p>
+                    </div>
+                    <div className="temp">
+                        {data.main ? <h1>{data.main.temp}</h1> : null}
+                    </div>
+                    <div className="description">
+                        {data.main ? <p>{data.weather[0].main}</p> : null}
+                    </div>
+                </div>
+                {data.name != undefined &&
+                    <div className="bottom">
+                        <div className="feels">
+                            {data.main ? <p className="bold">{data.main.feels_like}</p> : null}
+                            <p>Feels Like</p>
+                        </div>
+                        <div className="humidity">
+                            {data.main ? <p className="bold">{data.main.humidity}</p> : null}
+                            <p>Humidity</p>
+                        </div>
+                        <div className="wind">
+                            {data.main ? <p className="bold">{data.wind.speed}</p> : null}
+                            <p>Wind Speed</p>
+                        </div>
+                    </div>
+                }
+            </div>
+        </form>
+    );
 }
 
 export default App;
